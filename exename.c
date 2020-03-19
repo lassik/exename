21bebfb3ac050b1e26440656bf6fcb83294a1ac5
Lassi Kortela <lassi@lassi.io>
2020-03-19 12:51:28 +0200

    Use cap/len terminology from Go
    Capacity and length.

--- exename.c
+++ exename.c
@@ -5,35 +5,35 @@
 
 #ifdef _WIN32
 #include <windows.h>
-static int once(char *buf, size_t n)
+static int once(char *buf, size_t cap)
 {
-    DWORD nr = GetModuleFileNameA(NULL, buf, n);
-    if (!nr) {
+    DWORD len = GetModuleFileNameA(NULL, buf, cap);
+    if (!len) {
         return -1;
     }
-    return nr < n - 1;
+    return len < cap - 1;
 }
 #endif
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <mach-o/dyld.h>
-static int once(char *buf, size_t n)
+static int once(char *buf, size_t cap)
 {
-    uint32_t tmp = (uint32_t)n;
+    uint32_t tmp = (uint32_t)cap;
     return _NSGetExecutablePath(buf, &tmp) == 0;
 }
 #endif
 
 #ifdef __linux__
 #include <unistd.h>
-static int once(char *buf, size_t n)
+static int once(char *buf, size_t cap)
 {
-    ssize_t nr = readlink("/proc/self/exe", buf, n);
-    if (nr == (ssize_t)-1) {
+    ssize_t len = readlink("/proc/self/exe", buf, cap);
+    if (len == (ssize_t)-1) {
         return -1;
     }
-    if ((size_t)nr < n) {
-        buf[nr] = 0;
+    if ((size_t)len < cap) {
+        buf[len] = 0;
         return 1;
     }
     return 0;
@@ -56,10 +56,10 @@ static int mib[4] = { CTL_KERN, KERN_PROC_ARGS, -1, KERN_PROC_PATHNAME };
 #endif
 
 #if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
-static int once(char *buf, size_t n)
+static int once(char *buf, size_t cap)
 {
-    size_t nr = n;
-    if (sysctl(mib, 4, buf, &nr, 0, 0) == 0) {
+    size_t len = cap;
+    if (sysctl(mib, 4, buf, &len, 0, 0) == 0) {
         return 1;
     }
     return (errno == ENOMEM) ? 0 : -1;
@@ -72,16 +72,16 @@ char *exename(void)
 {
     char *buf;
     char *newbuf;
-    size_t n;
+    size_t cap;
     int ret;
 
     buf = NULL;
-    for (n = 64; n <= 16384; n *= 2) {
-        if (!(newbuf = realloc(buf, n))) {
+    for (cap = 64; cap <= 16384; cap *= 2) {
+        if (!(newbuf = realloc(buf, cap))) {
             break;
         }
         buf = newbuf;
-        ret = once(buf, n);
+        ret = once(buf, cap);
         if (ret == 1) {
             return buf;
         }
